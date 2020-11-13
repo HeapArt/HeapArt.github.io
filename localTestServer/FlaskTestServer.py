35825b0e7c2944fb58db738871d2a3a2750245d9
wlee0515 <wlee0515@users.noreply.github.com>
2020-11-12 22:59:36 -0500

    Page Setup

---
+++ localTestServer/FlaskTestServer.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+
+@app.route('/')
+def send_index():
+    return send_from_directory('../', 'index.html')
+    
+@app.route('/site/<path:path>')
+def send_site(path):
+    if path.endswith(".mjs"):
+        return send_from_directory('../site', path, as_attachment=True, mimetype='text/javascript')
+    else:
+        return send_from_directory('../site', path)
+
+
+
+def main():
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+if __name__ == '__main__':
+    main()
