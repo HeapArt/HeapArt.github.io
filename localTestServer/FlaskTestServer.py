92f8c0dfc6b7b92f1adba451137e445b6e4055e2
Willee <wilson.lee@cae.com>
2020-11-13 09:20:45 -0500

    up

--- localTestServer/FlaskTestServer.py
+++ localTestServer/FlaskTestServer.py
@@ -24,7 +24,7 @@ def main():
     scheduler.start()
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 if __name__ == '__main__':
     main()
