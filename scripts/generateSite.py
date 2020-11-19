ce35e648ed0e6c669e334de4a0eed36bc0ff16f7
Willee <wilson.lee@cae.com>
2020-11-19 16:40:37 -0500

    remade with out JS injected HTML

---
+++ scripts/generateSite.py
@@ -0,0 +1,245 @@
+import os
+import json
+
+gWorkingDirectory = "../"
+gInputFile = "scripts/sitePageDefinition.json"
+
+def loadJson(iFilePath):
+    with open(iFilePath) as f:
+        return json.load(f)
+    return NULL
+
+def generateSite(iSitePageDefinitionJSON, iSiteGenerationFile):
+    #print(iSitePageDefinitionJSON);
+    #print(iSiteGenerationFile);
+
+    for wPage in iSitePageDefinitionJSON["pages"]:
+        wTemp = {};
+        for wItem in iSitePageDefinitionJSON.items():
+          if wItem[0] != "pages":
+              if wItem[0] != "siteGenerationFile":
+                  wTemp[wItem[0]] = wItem[1]
+        
+        for wItem in wPage.items():
+            wTemp[wItem[0]] = wItem[1]
+        
+        generatePage(wTemp, iSiteGenerationFile)
+  
+
+def save_file(iPath, iText):
+    wBasedir = os.path.dirname(iPath)
+
+    if "" != wBasedir:
+        if not os.path.exists(wBasedir):
+            os.makedirs(wBasedir)
+
+    with  open(iPath, 'w') as wFile:
+        wFile.write(iText)
+        wFile.close()
+
+def generatePage(iPageDefinition, iSiteGenerationFile):
+
+    print(iPageDefinition)
+    wTemplateFile = open(iPageDefinition["template"], "r")
+    wFileString = wTemplateFile.read()
+    
+    js_scrpts = ""
+    if "js_list" in iSiteGenerationFile:
+        js_scrpts = ""
+        for wFile in iSiteGenerationFile["js_list"]:
+          js_scrpts += '<script src="{0}"></script>'.format(wFile)
+    wFileString = wFileString.replace("[%js_list%]", js_scrpts)
+    
+
+    css_links = ""
+    if "css_list" in iSiteGenerationFile:
+        for wFile in iSiteGenerationFile["css_list"]:
+          css_links += '<link rel="stylesheet" href="{0}">'.format(wFile)
+    wFileString = wFileString.replace("[%css_list%]", css_links)
+
+    if "tag_map" in iSiteGenerationFile:
+        for wTag in iSiteGenerationFile["tag_map"]:
+          wReplaceTag = wTag["tag"]
+          wParameter = ""
+
+          if "parameter" in wTag:
+              wParameter = iPageDefinition[wTag["parameter"]]
+          elif "file" in wTag:
+              wParameter = open(wTag["file"], "r").read()
+
+          wModifiedParameter = wParameter
+          if "method" in wTag:
+              wMethod = wTag["method"]
+              if "parse_navbar" == wMethod:
+                  wModifiedParameter = parse_navbar(wParameter, iPageDefinition)
+              if "parse_block" == wMethod:
+                  wModifiedParameter = parse_block(wParameter, iPageDefinition)
+              if "social_links_bar" == wMethod:
+                  wModifiedParameter = social_links_bar(wParameter, iPageDefinition)
+                  
+          wFileString = wFileString.replace("[%{0}%]".format(wReplaceTag), wModifiedParameter)
+
+    save_file(iPageDefinition["output_path"], wFileString)
+    print(wFileString)
+
+def generateNavBarEntryString(iEntry):
+    wTemp = ""
+    if "siteName" in iEntry:
+        if "icon" in iEntry:
+            wTemp += "<img src='{0}' class='class_navbar_icon_img'></img>".format(iEntry["icon"])
+
+        wTemp = "<div class='class_navbar_icon_container'>{0}</div>".format(wTemp)
+        wTemp += "<div class='class_navbar_label'>{0}</div>".format(iEntry["siteName"])
+        wTemp = "<div class='class_navbar_label_wrapper'>{0}</div>".format(wTemp)
+    
+        if "link" in iEntry:
+            wTemp = "<a href='{0}' class='class_navbar_link'>{1}</a>".format(iEntry["link"], wTemp)
+    
+        if "sub_entries" in iEntry:
+            for wSubEntry in iEntry["sub_entries"]:
+                wTemp += "<div class='class_navbar_subentry' onclick='navebarSubEntryExpand(this)' >" + generateNavBarEntryString(wSubEntry) + "</div>"
+
+    return wTemp
+
+
+def parse_navbar(iParameter, iPageDefinition):
+    wNavBar = loadJson(iParameter)
+
+    wInnerHtml = ""
+
+    if "siteName" in wNavBar:
+        wInnerHtml += "<div class='class_navbar_HomePage'>" + generateNavBarEntryString(wNavBar) + "</div>"
+
+    if "entries" in wNavBar:
+        wEntriesStr = ""
+
+        for wEntry in wNavBar["entries"]:
+            wEntriesStr += "<div class='class_navbar_entry'  onclick='navebarSubEntryExpand(this)'>" + generateNavBarEntryString(wEntry) + "</div>"
+
+        if "" != wEntriesStr:
+            wMenuIcon = '<div class="class_navbar_MenuIcon" onclick="navebarExpand(this)">'
+            wMenuIcon += '<div class="class_navbar_MenuIcon_bar1"></div>'
+            wMenuIcon += '<div class="class_navbar_MenuIcon_bar2"></div>'
+            wMenuIcon += '<div class="class_navbar_MenuIcon_bar3"></div>'
+            wMenuIcon += '</div>'
+
+        wInnerHtml +="<div class='class_navbar_entries'>" +  wMenuIcon + wEntriesStr + "</div>"
+
+    if "" != wInnerHtml:
+
+        wNavBar = "<div class='class_navbar'>" + wInnerHtml + "</div>"
+
+        wCode = 'function navebarExpand(iMenuButton) { iMenuButton.classList.toggle("navBarExpand");iMenuButton.parentElement.classList.toggle("navBarExpand");}'
+        wCode += 'function navebarSubEntryExpand(iEntry) {iEntry.classList.toggle("class_navbar_subentry_expand");}'
+        wScript = '<script>{0}</script>'.format(wCode)
+
+        return wNavBar + wScript
+
+    return ""
+
+
+def genBlockString(iBlock):
+    wTemp = ""
+    if "title" in iBlock:
+        wTemp += "<h2>{0}</h2>".format(iBlock["title"])
+  
+    if "date" in iBlock:
+        wTemp += "<p>{0}</p>".format(iBlock["date"])
+  
+    if "body" in iBlock:
+        wTemp += "<div class='class_site_block_body'>"
+        for wLine in  iBlock["body"]: 
+            wTemp += wLine
+        wTemp += "</div>"
+  
+  
+    if "caption" in iBlock:
+        wCaptionString = ""
+  
+        wType = iBlock["caption"]["type"]
+  
+        if "video" == wType:
+            if "videoId" in iBlock["caption"]:
+                wDOMString = '<iframe class="class_YouTubePlayer" src="https://www.youtube.com/embed/'
+                wDOMString += iBlock["caption"]["videoId"]
+                wDOMString += '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+                wCaptionString += wDOMString
+  
+            if "playlistId" in iBlock["caption"]:
+                wDOMString = '<iframe class="class_YouTubePlayer" src="https://www.youtube.com/embed/videoseries?list=PL'
+                wDOMString += iBlock["caption"]["playlistId"]
+                wDOMString += '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+                wCaptionString += wDOMString
+  
+            if "randomVideoId" in iBlock["caption"]:
+                wDOMString = '<iframe class="class_YouTubePlayer class_randomYTVideo"'
+                if "randomSeed" in iBlock["caption"]:
+                    wDOMString += ' randomSeed="{0}"'.format( iBlock["caption"]["randomSeed"])
+  
+                wDOMString += ' videoTags="{0}"'.format(iBlock["caption"]["randomVideoId"])
+                wDOMString += ' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+                wCaptionString += wDOMString
+  
+  
+        if "image" == wType:
+            if "src" in iBlock["caption"]:
+                wCaptionString += '<img class="class_block_image" src="{0}"/>'.format(iBlock["caption"]["src"])
+  
+        if "" != wCaptionString:
+            wTemp += "<div class='class_site_block_caption'>{0}</div>".format(wCaptionString)
+  
+    if "" != wTemp:
+        return "<div class='class_site_block'>{0}</div>".format(wTemp)
+  
+    return ""
+
+
+
+def parse_block(iParameter, iPageDefinition):
+    wBlockReverse = False
+    if "block_direction" in iPageDefinition:
+        if "reverse" == iPageDefinition["block_direction"]:
+            wBlockReverse = True
+
+    wBlockList = loadJson(iParameter)
+
+    if wBlockReverse:
+        wBlockList.reverse()
+
+    wBlockString = ""
+    for wBock in wBlockList:
+        wBlockString += genBlockString(wBock)
+
+    return '<div class="class_block_list">{0}</div>'.format(wBlockString)
+
+def social_links_bar(iParameter, iPageDefinition):
+    wSocialLinks = loadJson(iParameter)
+    
+    wLinks = ""
+    for wPlateform in wSocialLinks["links"]:
+        wLinks += '<a href="{0}" class="class_social_link"><img src="{1}" alt="{2}"/></a>'.format(wPlateform["link"],wPlateform["icon"],wPlateform["name"]);
+
+    return '<div class="class_social_links_bar">{0}</div>'.format(wLinks)
+
+def main():
+    os.chdir(gWorkingDirectory)
+
+    print(os.getcwd())
+
+    wSitePageDef = loadJson(gInputFile)
+    print("Using Site Page Definition : {0}".format(gInputFile))
+
+    wGenFile = wSitePageDef["siteGenerationFile"]
+    wSiteGenDef =  loadJson(wGenFile)
+    print("Using Site Generation File : {0}".format(wGenFile))
+
+    generateSite(wSitePageDef, wSiteGenDef)
+    
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- site/javascript/commonTasks.js
+++ site/javascript/commonTasks.js
@@ -37,64 +37,21 @@ function loadVideoList(iFunction){
 // Load Function
 window.onload = (event) => {
 
-  // External Style Links
-  var wHead = document.getElementsByTagName("head")[0];
-  wHead.innerHTML += '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">';
-  wHead.innerHTML += '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/v4-shims.css">';
-  wHead.innerHTML += '<link rel="stylesheet" href="/site/css/commonTask.css">';
-
-  // Create Navigation Bar
-  createNavigationBar("/site/database/navigation.json")
-
-  // Fill in Social media Links
-  var wSocialLinkDiv = document.getElementsByClassName("class_social_links_bar");
-  for (var wi = 0; wi < wSocialLinkDiv.length; ++wi) {
-    wSocialLinkDiv[wi].style.visibility = "hidden";
-    //  wSocialLinkDiv[wi].innerHTML += '<a href="https://www.youtube.com/c/heapart?sub_confirmation=1" class="fa fa-linkedin class_social_link"></a>';
-    wSocialLinkDiv[wi].innerHTML += '<a href="https://www.youtube.com/c/heapart" class="class_social_link"><img src="/site/images/logo/logo-youtube.png"/></a>';
-    wSocialLinkDiv[wi].innerHTML += '<a href="https://www.instagram.com/wlee0515/" class="class_social_link"><img src="/site/images/logo/logo-instagram.png"/></a>';
-    wSocialLinkDiv[wi].innerHTML += '<a href="https://www.tiktok.com/@heapart" class="class_social_link"><img src="/site/images/logo/logo-tiktok.png"/></a>';
-  }
-  
-  setTimeout(function() {
-    var wSocialLinkDiv = document.getElementsByClassName("class_social_links_bar");
-    for (var wi = 0; wi < wSocialLinkDiv.length; ++wi) {
-      wSocialLinkDiv[wi].style.visibility = "visible";
-    }
-  }, 200);
 
   // Add Event listener to resize YouTube Iframe
   window.addEventListener('resize', resizeYTIFrame);
-  setTimeout(resizeYTIFrame, 500);
-  console.log('page is fully loaded');
+  setTimeout(resizeYTIFrame, 100);
 
-  setInterval(iteration, 500);
+  
+  var wYTIFrame = document.getElementsByClassName(gClass_RandomYTVideo);
+  if (wYTIFrame.length > 0)
+  {
+    loadVideoList();
+  }
+
+  console.log('page is fully loaded');
 };
 
-// Iteration function
-function iteration() {
-
-  var wBlockList = document.getElementsByClassName(gClass_site_block);
-  if (null != wBlockList) {
-    for(var wi = 0; wi < wBlockList.length; ++wi) {
-      var wElementBlock = wBlockList[wi];
-      if ("visible" != wElementBlock.style.visibility)
-      {
-        if( true == isInViewport(wElementBlock)) {
-          wElementBlock.style.visibility="visible";
-          break;
-        }  
-      }
-    }
-  }
-}
-
-// Standard Functions
-function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return ( rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.left <= (window.innerWidth || document.documentElement.clientWidth));
-}
 
 function getVideoWithTags(iTagList, iVideoList) {
   var wNewVideoList = new Array();
@@ -158,187 +115,4 @@ function getDayCount() {
   var d = new Date();
   var n = d.getTime(); //milli seconds
   return Math.floor(n/(1000*3600*24));
-}
-
-// NavBar Generation
-function generateEntryString(iJson) {
-
-  var wTemp = "";
-
-  if (null != iJson.siteName) {
-
-    if (null != iJson.icon) {
-      wTemp = "<img src='" + iJson.icon + "' class='class_navbar_icon_img'></img>";
-    }
-    wTemp = "<div class='class_navbar_icon_container'>" + wTemp + "</div>";
-    wTemp += "<div class='class_navbar_label'>" + iJson.siteName + "</div>";
-    wTemp = "<div class='class_navbar_label_wrapper'>" + wTemp + "</div>";
-    
-    if (null != iJson.link) {
-      wTemp = "<a href='" + iJson.link + "' class='class_navbar_link'>" + wTemp + "</a>";
-    }
-    
-
-    if (null != iJson.sub_entries) {
-      for (var wj = 0; wj < iJson.sub_entries.length; ++wj) {
-        wTemp += "<div class='class_navbar_subentry' onclick='navebarSubEntryExpand(this)' >" + generateEntryString(iJson.sub_entries[wj]) + "</div>";
-      }   
-    }
-  }
-
-  return wTemp;
-}
-
-function navebarExpand(iMenuButton) {
-  iMenuButton.classList.toggle("navBarExpand");
-  iMenuButton.parentElement.classList.toggle("navBarExpand");
-}
-
-function navebarSubEntryExpand(iEntry) {
-  iEntry.classList.toggle("class_navbar_subentry_expand");
-}
-
-
-function createNavigationBar(iFetchJson) {
-  fetch(iFetchJson)
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      var wInnerHtml = "";
-
-      if (null != data.siteName) {
-        wInnerHtml += "<div class='class_navbar_HomePage'>" + generateEntryString(data) + "</div>";
-      }
-      if (null != data.entries) {
-        var wEntriesStr = "";
-        for (var wi = 0; wi < data.entries.length; ++wi) {
-          wEntriesStr += "<div class='class_navbar_entry' onclick='navebarSubEntryExpand(this)'>" + generateEntryString(data.entries[wi]) + "</div>";
-        }
-
-        if ("" != wEntriesStr) {
-
-          var wMenuIcon = '<div class="class_navbar_MenuIcon" onclick="navebarExpand(this)">';
-          wMenuIcon += '<div class="class_navbar_MenuIcon_bar1"></div>';
-          wMenuIcon += '<div class="class_navbar_MenuIcon_bar2"></div>';
-          wMenuIcon += '<div class="class_navbar_MenuIcon_bar3"></div>';
-          wMenuIcon += '</div>';
-
-          wInnerHtml +="<div class='class_navbar_entries'>" +  wMenuIcon + wEntriesStr + "</div>";
-        }
-      }
-
-      if ("" != wInnerHtml) {
-
-        var wNavBar = "<div class='class_navbar'>" + wInnerHtml + "</div>";
-        document.body.innerHTML = "<div class='class_navbar_gap'>' + '</div>" +  document.body.innerHTML + wNavBar;
-      }
-    })
-    .catch((err) => {
-      // Do something for an error here
-    })
-}
-
-
-// Block Generation
-
-function genBlockString(iJson) {
-  var wTemp = "";
-  if (null != iJson.title) wTemp += "<h2>" + iJson.title + "</h2>";
-  if (null != iJson.date) wTemp += "<p>" + iJson.date + "</p>";
-  if (null != iJson.body) {
-    wTemp += "<div class='class_site_block_body'>";
-    for (var wj = 0; wj < iJson.body.length; ++wj) {
-      wTemp += iJson.body[wj];
-    }
-    wTemp += "</div>";
-  }
-
-  if (null != iJson.caption) {
-    if ("video" == iJson.caption.type) {
-      if (null != iJson.caption.videoId) {
-        var wVideoId = iJson.caption.videoId
-        var wDOMString = '<iframe class="' + gClass_YouTubePlayer + '" src="https://www.youtube.com/embed/'
-        wDOMString += wVideoId;
-        wDOMString += '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-        wTemp += wDOMString;
-      }
-      else if (null != iJson.caption.playlistId) {
-        var wPlayListId = iJson.caption.playlistId
-        var wDOMString = '<iframe class="' + gClass_YouTubePlayer + '" src="https://www.youtube.com/embed/videoseries?list=PL'
-        wDOMString += wPlayListId;
-        wDOMString += '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-        wTemp += wDOMString;
-      }
-      else if (null != iJson.caption.randomVideoId) {
-
-        var wDOMString = '<iframe class="' + gClass_YouTubePlayer + " " + gClass_RandomYTVideo + '" ';
-        
-        if (null != iJson.caption.randomSeed) {
-          wDOMString += ' randomSeed="'+ iJson.caption.randomSeed +'" ';
-        }
-
-        wDOMString += 'videoTags="' + iJson.caption.randomVideoId + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-        
-        loadVideoList();
-        wTemp += wDOMString;
-      }
-    }
-    else if ("image" == iJson.caption.type) {
-      if (null != iJson.caption.src) {
-        var wDOMString = '<img class="class_block_image" src="'+ iJson.caption.src + '"/>';
-        wTemp += wDOMString;
-      }    
-    }
-  }
-
-  if ("" != wTemp){
-    return wTemp;
-  }
-  return "";
-}
-
-function loadSiteBlock(iElementId, iFetchJson, iReverse) {
-  var wElement = document.getElementById(iElementId);
-  if (null != wElement) {
-    fetch(iFetchJson)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        var wCount = data.length;
-        if (0 != wCount) {
-
-          if ((null == iReverse) || (false == iReverse)) {
-            for (var wi = 0; wi < wCount; ++wi) {
-              var wBlockString = genBlockString(data[wi]);
-              if (""!= wBlockString) {
-                var wNewDiv = document.createElement("DIV");
-                wNewDiv.classList.add(gClass_site_block);
-                wNewDiv.innerHTML = wBlockString;
-                wNewDiv.style.display = "block";
-                wNewDiv.style.visibility = "hidden";
-                wElement.appendChild(wNewDiv);  
-              }
-            }  
-          }
-          else {
-            for (var wi = wCount - 1; wi >= 0; --wi) {
-              var wBlockString = genBlockString(data[wi]);
-              if (""!= wBlockString) {
-                var wNewDiv = document.createElement("DIV");
-                wNewDiv.classList.add(gClass_site_block);
-                wNewDiv.innerHTML = wBlockString;
-                wNewDiv.style.display = "block";
-                wNewDiv.style.visibility = "hidden";
-                wElement.appendChild(wNewDiv);  
-              }
-            }  
-          }
-        }
-      })
-      .catch((err) => {
-        // Do something for an error here
-      })
-  }
 }
