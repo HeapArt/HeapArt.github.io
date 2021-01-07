58cb6d07a1155e6981ad4c1eb29303a96a18669f
wlee0515 <wlee0515@users.noreply.github.com>
2021-01-06 22:56:30 -0500

    Update commonTasks.js

--- site/javascript/commonTasks.js
+++ site/javascript/commonTasks.js
@@ -89,7 +89,7 @@ function fillRandomYTVideoPlayers(iVideoList)
     var wList = iVideoList;
     var wTags = wDom.getAttribute("videoTags");
     if (null != wTags) {
-      wList = getVideoWithTags(wTags.split(","),iVideoList[""]);
+      wList = getVideoWithTags(wTags.split(","),iVideoList);
     }
     var wSeed = wDom.getAttribute("randomSeed");
     var wRandom = Math.random()*wList.length;
